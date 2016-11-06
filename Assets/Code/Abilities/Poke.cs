9825e36d0aaf6a6fe9a22d5b0eb2e645d01bf013
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-11-05 23:03:10 -0200

    Poke now hits enemy

--- Assets/Code/Abilities/Poke.cs
+++ Assets/Code/Abilities/Poke.cs
@@ -4,16 +4,16 @@ using Assets.Code.Interfaces;
 
 public class Poke : Ability
 {
-	public GameObject pokeBulletPrefab;
+	public PokeBullet pokeBulletPrefab;
 
     protected override void Cast(AbilityCastEventArgs e)
-    { 
-		
+    {
 		Vector3 direction = FindObjectOfType<BossCharacter>().transform.position - transform.position;
         float directionAngle = Vector3.Angle(direction, new Vector3(1, 0, 0));
 
-        GameObject bullet = Instantiate(pokeBulletPrefab);
-		bullet.transform.position = transform.position;
+        var bullet = Instantiate(pokeBulletPrefab);
+        bullet.Target = FindObjectOfType<BossCharacter>().gameObject;
+        bullet.transform.position = transform.position;
 		bullet.transform.rotation = Quaternion.Euler(0,0,directionAngle);
 
         Cooldown.ResetCooldown();
---
+++ Assets/Code/Mechanics/PokeBullet.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PokeBullet : MonoBehaviour {
+
+	[SerializeField]
+	private float _pokeVelocity = 10f;
+
+	[SerializeField]
+	private float _damageDone = 10;
+
+	[SerializeField]
+	private float _timeToDamage = 1;
+
+    [SerializeField]
+    private float _timeToDestroy = 0.5f;
+
+    private bool _hitTarget = false;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (_hitTarget) return;
+		transform.localPosition += transform.right * _pokeVelocity * Time.deltaTime; //Bullet going in boss direction (your own right direction)
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Barrier>())
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.gray;
+            _hitTarget = true;
+            StartCoroutine(Destroy());
+        }
+        if (other.gameObject != Target) return;
+
+        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        _hitTarget = true;
+        var targetChar = other.transform.GetComponent<Character>();
+        if (targetChar)
+        {
+            targetChar.GetComponent<SpriteRenderer>().color = Color.gray;
+            StartCoroutine(DoDamage(targetChar));
+        }
+    }
+
+    IEnumerator DoDamage(Character targetChar)
+	{
+		yield return new WaitForSeconds (_timeToDamage);
+		targetChar.Health.Damage(_damageDone);
+		targetChar.GetComponent<SpriteRenderer>().color = Color.white;
+        StartCoroutine(Destroy());
+    }
+
+    IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(_timeToDestroy);
+        Destroy(gameObject);
+    }
+
+
+
+    public GameObject Target { set; private get; }
+}
+		
